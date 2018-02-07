3be35b5858d3fc6c02d0ffc269df743b978d8878
Jonni Lundy <jonathan.lundy@liferay.com>
2018-02-06 16:31:13 -0800

    Changes font weight

--- pages/index.vue
+++ pages/index.vue
@@ -40,14 +40,14 @@ export default {
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
-  font-weight: 300;
+  font-weight: 100;
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
 .subtitle {
-  font-weight: 300;
+  font-weight: 100;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
